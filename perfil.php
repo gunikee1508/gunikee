5287884307cfbb2077f456b4c7a4cc02ac09a1da
gunikee1508 <57869814+gunikee1508@users.noreply.github.com>
2019-11-17 13:26:16 -0300

    Create perfil.php

---
+++ perfil.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+<?php
+session_start();
+if((!isset ($_SESSION['email']) == true) and (!isset ($_SESSION['senha']) == true) and (!isset ($_SESSION['nor']) == true))
+{
+  unset($_SESSION['email']);
+  unset($_SESSION['senha']);
+  unset($_SESSION['nome']);
+  unset($_SESSION['nor']);
+  header('location:../visitante/login.php');
+  }
+?>
+    <meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+    <title>Troca de senha</title>
+    <link rel="stylesheet" type="text/css" href="estrutura/estrutura.css" />
+    <link rel="stylesheet" type="text/css" href="sair/sair.css" />
+    <link rel="shortcut icon" href="./imagens/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="rodape/rodape.css" />
+    <link rel="stylesheet" type="text/css" href="./butpesquisa.css" />
+     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Cherry+Swash'>
+      <link rel="stylesheet" href="icones/icones.css">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="../testemenu/css/styles.css">
+        <script src="testemenu/js/responsive-nav.js"></script>
+
+</head>
+
+<body>
+<style>
+  
+
+  @media all and (max-width : 1200px) {
+
+.logo {
+  width:10%;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+  text-decoration: none;
+  font-weight: bold;
+  line-height: 55px;
+  padding: 0 20px;
+  color: #fff;
+  float: left;
+}
+  }
+</style>
+
+
+<header>
+      <a href="../index2.php" class="logo"  data-scroll><img src="../imagens/190x33.png" ></a>
+       <ul class="nav">
+      <nav class="nav-collapse">
+        <ul>
+          <li class="menu-item active"><a href="../index2.php" data-scroll>Inicio</a></li>
+          <li class="menu-item"><a href="./enem.php" data-scroll>ENEM</a></li>
+          <li class="menu-item"><a href="../menu/materias.php" data-scroll>Matérias</a></li>
+          <li class="menu-item"><a href="../menu/contato2.php" data-scroll>Contato</a></li>
+          <li class="menu-item"><a href="../logout.php" onclick="return confirm('Deseja realmente sair?');" title="Deslogar">SAIR</a></li>
+        </ul>
+      </ul>
+      </nav>
+    </header>
+
+
+    <script src="testemenu/js/fastclick.js"></script>
+    <script src="testemenu/js/scroll.js"></script>
+    <script src="testemenu/js/fixed-responsive-nav.js"></script>
+<br><br>
+
+    <br>
+    <div class="rede" id="facebook" id="alinhar">
+     <img class="icone" src="imagens/facebook.png">
+    </div>
+    <div class="rede" id="twitter" id="alinhar">
+     <img class="icone" src="imagens/twitter.png">
+    </div>
+    <div class="rede" id="instagram" id="alinhar">
+     <img class="icone" src="imagens/instagram.png">
+    </div>
+    <div class="rede" id="youtube" id="alinhar">
+     <img class="icone" src="imagens/youtube.png">
+    </div>
+  </section>
+
+
+
+
+
+
+<style>
+
+<br><br><br>
+.conteudoos {
+  overflow: hidden;
+  position: absolute;
+  margin-left: 30%;
+  margin-right: 30%;
+  top: 20%;
+  color: #000000;
+
+}
+
+.conteudoos h2 {
+  text-align: center;
+  font-size: 45px;
+  font-family: Geneva;
+  color:#FF8000;
+}
+
+.esquerda ul {
+  list-style: none;
+  padding: 1em 3em;
+}
+
+.esquerda ul li {
+  display: inline-block;
+}
+
+.esquerda ul li a {
+  text-decoration: none;
+  color: #000;
+  font-weight: 500;
+  padding: 8px 16px;
+  transition: all .2s ease;
+  font-family: "Glyphicons Halflings Regular";
+
+}
+
+.esquerda a:hover {
+    text-decoration: none;
+  border: none;
+  color: rgba(30, 41, 255, .9);
+}
+
+.button3 {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+}
+
+
+.button4:hover {
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+}
+
+</style>
+
+<script>
+  function mascara(i){
+   
+   var v = i.value;
+   
+   if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+      i.value = v.substring(0, v.length-1);
+      return;
+   }
+   
+   i.setAttribute("maxlength", "14");
+   if (v.length == 3 || v.length == 7) i.value += ".";
+   if (v.length == 11) i.value += "-";
+
+}
+</script>
+
+<br><br>
+
+<form action="../visitante/senhanova.php" method="post">
+
+<div class="conteudoos">
+  <h2>TROCAR SENHA</h2>
+        </div>
+        <br>
+<center>
+<div id='busca'>
+            
+          <input oninput="mascara(this)" type="text"  id="txtbusca" name="cpf" placeholder="Digite seu cpf" size="14"> 
+<br><br>
+<br><br>
+          <input type="password" id="txtbusca"  name="nsenha" placeholder="Digite sua senha nova" size="50"> 
+
+</div>
+<br>
+<button class="button3 button4">Confirmar</button>
+
+<br><br><br><br><br><br><br><br><br><br><br>
+<div id= "corpo_pagina">
+  <font color="white">
+      <center>© Todos os direitos autorais ao grupo Focus.</center>
+      <h3><center>Contate-nos: projsemduvidas@gmail.com</center></h3>
+      <center><h5>Lemos de Castro, 2019.</h5></center>
+    </font>
+</div>
+
+</body>
+</html>
